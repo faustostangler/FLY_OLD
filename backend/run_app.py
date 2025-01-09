e7aa7cdf65915e4f6dbcfc159a998dfbd09bb4e9
Fausto Stangler (Gamer) <fausto.stangler@gmail.com>
2025-01-08 21:21:21 -0300

    pre PK and sql fix

--- backend/run_app.py
+++ backend/run_app.py
@@ -7,41 +7,41 @@ from utils.stock_processor import StockProcessor
 if __name__ == '__main__':
     try:
         # Ask the user if they want to get company information
-        company_processor = CompanyProcessor()
         run_company_processor = 'N'
         prompt = 'Want to update company information? (YES/NO): '
         # run_company_processor = company_processor.timed_input(prompt)
         if run_company_processor.strip().upper().startswith('Y'):
+            company_processor = CompanyProcessor()
             company_processor.main(thread=True)
-        company_processor.close_driver()
+            company_processor.close_driver()
 
         # Ask the user if they want to get nsd information
-        nsd_processor = NsdProcessor()
         run_nsd_processor = 'N'
         prompt = 'Want to update nsd information? (YES/NO): '
         # run_nsd_processor = nsd_processor.timed_input(prompt)
         if run_nsd_processor.strip().upper().startswith('Y'):
+            nsd_processor = NsdProcessor()
             nsd_processor.main(thread=True)
-        nsd_processor.close_driver()
+            nsd_processor.close_driver()
 
         # Ask the user if they want to get finantial statements
-        statements_processor = StatementsProcessor()
-        statements_processor.close_driver()
         run_statements_processor = 'N'
         prompt = 'Want to update statements information? (YES/NO): '
         # run_statements_processor = statements_processor.timed_input(prompt)
         if run_statements_processor.strip().upper().startswith('Y'):
+            statements_processor = StatementsProcessor()
+            statements_processor.close_driver()
             statements_processor.main(thread=True)
-        statements_processor.close_driver()
+            statements_processor.close_driver()
 
         # Ask the user if they want to get finantial historical market data from yfinance
-        market_processor = MarketProcessor()
         run_market_processor = 'Y'
         prompt = 'Want to update markets historical data? (YES/NO): '
         # run_market_processor = market_processor.timed_input(prompt)
         if run_market_processor.strip().upper().startswith('Y'):
+            market_processor = MarketProcessor()
             market_processor.main(thread=True)
-        market_processor.close_driver()
+            market_processor.close_driver()
 
         # Ask the user if they want to get finantial historical market data direct form b3 source
         stock_processor = StockProcessor()
--- backend/utils/market_processor.py
+++ backend/utils/market_processor.py
@@ -18,7 +18,8 @@ class MarketProcessor(BaseProcessor):
         The main method to scrape NSD data, parse it, and save it to the database.
         """
         try:
-            pass
+            statements_data = self.load_data(table_name=self.config.statements_file, db_filepath=self.config.initial_filepath)
+            self.save_to_db(dataframe=statements_data[:10000], table_name=self.config.statements_file, db_filepath=r'd:\\Fausto Stangler\\Documentos\\Python\\FLY\\backend\\data\\statements initial market.db')
 
         except Exception as e:
             self.logerror(e)
