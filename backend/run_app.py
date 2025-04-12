905b2f3d25c1e405c89330b8a44f9999294d603a
Fausto Stangler <fausto.stangler@gmail.com>
2025-04-12 10:36:05 -0300

    gamer commit

--- backend/run_app.py
+++ backend/run_app.py
@@ -6,12 +6,16 @@ from utils.intel_processor import IntelProcessor
 from utils.historical_stock_url_processor import HistoricalStockUrlProcessor
 from utils.corporate_events_processor import CorporateEventsProcessor, EventsStatementsProcessor
 
+from utils.base_processor import BaseProcessor
+
 if __name__ == '__main__':
+    base = BaseProcessor()
+
     try:
         # Ask the user if they want to get company information
         run_company_processor = 'N'
         prompt = 'Want to update company information? (YES/NO): '
-        # run_company_processor = company_processor.timed_input(prompt)
+        run_company_processor = base.timed_input(prompt)
         if run_company_processor.strip().upper().startswith('Y'):
             company_processor = CompanyProcessor()
             company_processor.main(thread=True)
@@ -20,7 +24,7 @@ if __name__ == '__main__':
         # Ask the user if they want to get nsd information
         run_nsd_processor = 'N'
         prompt = 'Want to update nsd information? (YES/NO): '
-        # run_nsd_processor = nsd_processor.timed_input(prompt)
+        # run_nsd_processor = base.timed_input(prompt)
         if run_nsd_processor.strip().upper().startswith('Y'):
             nsd_processor = NsdProcessor()
             nsd_processor.main(thread=True)
@@ -29,7 +33,7 @@ if __name__ == '__main__':
         # Ask the user if they want to get finantial statements
         run_statements_processor = 'N'
         prompt = 'Want to update statements information? (YES/NO): '
-        # run_statements_processor = statements_processor.timed_input(prompt)
+        # run_statements_processor = base.timed_input(prompt)
         if run_statements_processor.strip().upper().startswith('Y'):
             statements_processor = StatementsProcessor()
             statements_processor.close_driver()
@@ -40,7 +44,7 @@ if __name__ == '__main__':
         # Ask the user if they want to sstandardize the statements
         run_intel_processor = 'Y'
         prompt = 'Want to standardize statements information? (YES/NO): '
-        # run_statements_processor = statements_processor.timed_input(prompt)
+        # run_statements_processor = base.timed_input(prompt)
         if run_intel_processor.strip().upper().startswith('Y'):
             intel_processor = IntelProcessor()
             intel_processor.main(thread=False)
@@ -51,7 +55,7 @@ if __name__ == '__main__':
         # Ask the user if they want to get corporate events from b3
         run_corporate_events_processor = 'Y'
         prompt = 'Want to update corporate events? (YES/NO): '
-        # run_corporate_events_processor = corporate_events_processor.timed_input(prompt)
+        # run_corporate_events_processor = base.timed_input(prompt)
         if run_corporate_events_processor.strip().upper().startswith('Y'):
             # corporate_events_processor = CorporateEventsProcessor()
             # corporate_events_processor.main(thread=True)
@@ -66,7 +70,7 @@ if __name__ == '__main__':
         # stock_processor = StockProcessor()
         # run_stock_processor = 'Y'
         # prompt = 'Want to update stock historical data? (YES/NO): '
-        # # run_stock_processor = stock_processor.timed_input(prompt)
+        # # run_stock_processor = base.timed_input(prompt)
         # if run_stock_processor.strip().upper().startswith('Y'):
         #     stock_processor.main(thread=True)
         # stock_processor.close_driver()
